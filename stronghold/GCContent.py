9dac1a2276e3336f9ede03e217a303ac294b0689
Nick Garbalau <nick.g@worldviewltd.com>
2024-08-25 17:35:30 +0200

    fix: more fixs

--- stronghold/GCContent.py
+++ stronghold/GCContent.py
@@ -4,9 +4,34 @@ def readFile(filePath):
         return [l.strip() for l in file.readlines()]
     
 def gc_content(seq):
+    """
+    GC Content in a DNA/RNA sequence
+    """
     return round(((seq.count('C') + seq.count('G')) / len(seq) * 100), 6)
 
-# Clean & prepare data (format using gc_content func)
+# Storing our data
+FASTAFile = readFile('../test_data/gc_content.txt')
+FASTADict = {}
+FASTALabel = ''
+
+# print(FASTAFile)
+
+for line in FASTAFile:
+    if '>' in line:
+        FASTALabel = line
+        FASTADict[FASTALabel] = ''
+    else:
+        FASTADict[FASTALabel] = line
+
 # Format the data (store the data in a convinient way)
 # Run needed operation on the data (pass to gc_content)
+ResultDict = {
+    key: gc_content(value) for (key, value) in FASTADict.items()
+}
+print(ResultDict)
+
 # Collect results (Rosalind submission in our case)
+MaxGCKey = max(ResultDict, key=ResultDict.get)
+
+# >Rosalind_0808 -> 59.259259
+print(f'{MaxGCKey[1:]}\n{ResultDict[MaxGCKey]}')
